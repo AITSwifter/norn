953aeb8091454defbb000a0076e1b24d5effa0b6
nissy-yuki <nisi20010510@gmail.com>
2021-10-12 16:21:43 +0900

    add 行き先 & numonly

--- norn/norn/ContentView.swift
+++ norn/norn/ContentView.swift
@@ -73,8 +73,10 @@ struct InputView: View{
     @Environment(\.managedObjectContext) private var context
     @Environment(\.presentationMode) var presentationMode
     @State private var name = ""
-    @State private var Ttime = 0
+    @State private var destination = ""
+    @State private var Ttime: Int16?
     @State private var nameeditting = false
+    @State private var desteditting = false
     @State private var Ttimeeditting = false
     @State private var showingAlert = false
     var body: some View{
@@ -88,26 +90,46 @@ struct InputView: View{
                     UIApplication.shared.closeKeyboard()
                 }
             VStack{
-                //駅名の入力欄
-                TextField("駅名",text: $name,
-                          onEditingChanged: { begin in
-                            /// 入力開始処理
-                            if begin {
-                                self.nameeditting = true
-                                // 編集フラグをオン
-                                /// 入力終了処理
-                            } else {
-                                self.nameeditting = false   // 編集フラグをオフ
-                            }
-                          })
-                    //入力中に枠を青く強調表示
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding()
-                    // 編集フラグがONの時に枠に影を付ける
-                    .shadow(color: nameeditting ? .blue : .clear, radius: 3)
+                HStack{
+                    //駅名の入力欄
+                    TextField("駅名",text: $name,
+                              onEditingChanged: { begin in
+                                /// 入力開始処理
+                                if begin {
+                                    self.nameeditting = true
+                                    // 編集フラグをオン
+                                    /// 入力終了処理
+                                } else {
+                                    self.nameeditting = false   // 編集フラグをオフ
+                                }
+                              })
+                        //入力中に枠を青く強調表示
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                        // 編集フラグがONの時に枠に影を付ける
+                        .shadow(color: nameeditting ? .blue : .clear, radius: 3)
+                    //駅名の入力欄
+                    TextField("行き先",text: $destination,
+                              onEditingChanged: { begin in
+                                /// 入力開始処理
+                                if begin {
+                                    self.desteditting = true
+                                    // 編集フラグをオン
+                                    /// 入力終了処理
+                                } else {
+                                    self.desteditting = false   // 編集フラグをオフ
+                                }
+                              })
+                        //入力中に枠を青く強調表示
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                        // 編集フラグがONの時に枠に影を付ける
+                        .shadow(color: desteditting ? .blue : .clear, radius: 3)
+                    
+                }
                 
                 //時刻表の入力欄
-                TextField("時刻表",text: $Ttime.IntToStrDef(0),
+                TextField("時刻表",value: $Ttime, formatter: NumberFormatter(),
                           onEditingChanged: { begin in
                             /// 入力開始処理
                             if begin {
@@ -118,6 +140,7 @@ struct InputView: View{
                                 self.Ttimeeditting = false   // 編集フラグをオフ
                             }
                           })
+                    .keyboardType(.numberPad)
                     //入力中に枠を青く強調表示
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding()
@@ -148,11 +171,11 @@ struct InputView: View{
         
     }
     
-    func savetable(text: String, num: Int){
+    func savetable(text: String, num: Int16){
         /// 時刻表新規登録処理
         let newTtable = Timetable(context: context)
         newTtable.name = text
-        newTtable.timetable = Int16(num)
+        newTtable.timetable = num
         newTtable.timestamp = Date()
         try? context.save()
     }
