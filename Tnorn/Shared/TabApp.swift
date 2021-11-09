adf9d4bd2a8ecd9f994856af3842559662341a58
k20028kk <k20028kk@k20028kknoMacBook-Air.local>
2021-11-09 15:57:33 +0900

    hi

---
+++ Tnorn/Shared/TabApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabApp.swift
+//  Tab
+//
+//  Created by k20028kk on 2021/10/13.
+//
+
+import SwiftUI
+
+@main
+struct TabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tnorn/Shared/UpperTabView.swift
@@ -0,0 +1,39 @@
+//
+//  UpperTabView.swift
+//  Tab
+//
+//  Created by k20028kk on 2021/10/19.
+//
+
+import SwiftUI
+
+struct UpperTabView: View {
+    // ボタンのタップで状態を変える 親Viewに状態を伝えるため @Binding をつけておく
+    @Binding var selection: TabType
+    let geometrySize: CGSize
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: .zero) {
+            HStack(spacing: .zero) {
+                Button(action: {
+                    self.selection = .timetable
+                }, label: {
+                    Text("時刻表")
+                        .font(.headline)
+                })
+                .frame(width: geometrySize.width / 2, height: 44.0)
+                Button(action: {
+                    self.selection = .mytimetable
+                }, label: {
+                    Text("プリセット")
+                        .font(.headline)
+                })
+                .frame(width: geometrySize.width / 2, height: 44.0)
+            }
+            Rectangle()
+                .frame(width: geometrySize.width / 2, height: 2.0)
+                .offset(x: self.selection == .timetable ? .zero: geometrySize.width / 2, y: .zero)
+        }
+    }
+}
+
