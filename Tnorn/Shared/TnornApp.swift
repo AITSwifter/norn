596ffcb8eb1777aff7e605285c4a6fe887f3f79e
わだたける <wdtkru@icloud.com>
2021-11-26 15:00:30 +0900

    appの元のコード追加

--- Tnorn/Shared/TnornApp.swift
+++ Tnorn/Shared/TnornApp.swift
@@ -18,3 +18,18 @@ struct TnornApp: App {
         }
     }
 }
+
+//
+//import SwiftUI
+//
+//@main
+//struct TnornApp: App {
+//    let persistenceController = PersistenceController.shared
+//
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+//        }
+//    }
+//}
